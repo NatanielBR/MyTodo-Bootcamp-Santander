b39269d9b518f6f5bc483a8d2a2be4b09e39726d
Nataniel <natannada08@gmail.com>
2021-08-23 10:38:03 -0300

    Desativado dark-mode

--- app/src/main/java/com/natanielbr/mytodo/MainActivity.kt
+++ app/src/main/java/com/natanielbr/mytodo/MainActivity.kt
@@ -1,22 +1,15 @@
 package com.natanielbr.mytodo
 
-import android.media.RingtoneManager
+import android.content.res.Configuration
 import android.os.Bundle
-import android.view.Menu
-import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
-import androidx.fragment.app.FragmentContainerView
-import androidx.lifecycle.lifecycleScope
-import androidx.navigation.NavDirections
+import androidx.appcompat.app.AppCompatDelegate
 import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
-import com.google.android.material.snackbar.Snackbar
 import com.natanielbr.mytodo.databinding.ActivityMainBinding
-import com.natanielbr.mytodo.ui.fragments.TodoHomeFragment
-import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
 
@@ -43,6 +36,8 @@ class MainActivity : AppCompatActivity() {
 
         appBarConfiguration = AppBarConfiguration(navController.graph)
         setupActionBarWithNavController(navController, appBarConfiguration)
+
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
 
     }
 
